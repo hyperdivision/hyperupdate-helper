611b5a793322745e487e67dd59565982ff17b910
Mathias Buus <mathiasbuus@gmail.com>
2020-07-17 16:32:18 +0200

    fix path

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const path = require('path')
-module.exports = path.resolve('prebuilds/' + process.platform + '-' + process.arch + (process.platform === 'win32' ? '.exe' : ''))
+module.exports = path.join(__dirname, 'prebuilds/' + process.platform + '-' + process.arch + (process.platform === 'win32' ? '.exe' : ''))
